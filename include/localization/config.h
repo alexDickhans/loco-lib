8de38248b9df9a288b42d78aca1169fadc97585d
Alex Dickhans <adickhans@gmail.com>
2024-10-04 17:26:25 -0600

    document configuration file

--- include/localization/config.h
+++ include/localization/config.h
@@ -1,10 +1,38 @@
 #pragma once
 
-namespace LOCO_CONFIG {
-	constexpr size_t LINE_SENSOR_THRESHOLD = 2000;
-	constexpr QLength LINE_SENSOR_DISTANCE_THRESHOLD = 3_in;
+/**
+ * @brief Static class to hold configuration values for the particle filter. Generally, these values are a good
+ * starting point and will work in most use cases, but that can also be changed as needed.
+ */
+class LOCO_CONFIG {
+public:
+    /**
+     * @brief Sensor threshold for determining if the line sensor is over a line. This is a good value at approximately
+     * 7mm from the tiles, which we found was a good balance between packaging and sensor SNR.
+     */
+    static constexpr size_t LINE_SENSOR_THRESHOLD = 2000;
 
-	constexpr float DISTANCE_WEIGHT = 1.0;
-	constexpr float GPS_WEIGHT = 1.0;
-	constexpr float LINE_WEIGHT = 1.0;
-}
+    /**
+     * @brief Distance from the CENTER of the line to predict the value to be positive. Largely untested, but upon more
+     * testing this value will be updated appropriately.
+     */
+    static constexpr QLength LINE_SENSOR_DISTANCE_THRESHOLD = 1_in;
+
+    /**
+     * @brief Weight for the distance sensor. Higher means it will have a larger impact on the particle filter, lower
+     * means a smaller impact.
+     */
+    static constexpr float DISTANCE_WEIGHT = 1.0;
+
+    /**
+     * @brief Weight for the game positioning sensor. Higher means it will have a larger impact on the particle filter, lower
+     * means a smaller impact.
+     */
+    static constexpr float GPS_WEIGHT = 1.0;
+
+    /**
+     * @brief Weight for the line sensor. Higher means it will have a larger impact on the particle filter, lower
+     * means a smaller impact.
+     */
+    static constexpr float LINE_WEIGHT = 1.0;
+};
